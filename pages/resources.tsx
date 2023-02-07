79b4a443f2215c5e3c1f74114c135abd2b1b4a6f
Liz <Liz5112@users.noreply.github.com>
2023-02-06 20:33:16 -0600

    Add buttons to resources page

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,9 +1,27 @@
-import { Container } from "@chakra-ui/react";
+import { Container, Stack, Button, Link } from "@chakra-ui/react";
 
 function Resources() {
   return (
     <Container maxW="xl">
       <h1>Language Learning Resources</h1>
+      <Stack spacing={4} direction="row" align="center">
+        <Link href="/resources/category1">
+          <Button
+            bg="purple3"
+            color="white"
+            size="sm">
+            Category 1
+          </Button>
+        </Link>
+        <Link href="/resources/category2">
+          <Button
+            bg="teal3"
+            color="white"
+            size="sm">
+            Category 2
+          </Button>
+        </Link>
+      </Stack>
     </Container>
   );
 }

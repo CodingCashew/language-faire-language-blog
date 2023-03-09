152691c913065c64c0cc476180529556096034f5
lizblackledge <lizblackledge@users.noreply.github.com>
2023-03-09 08:24:38 -0600

    Create PDF page, render cards dynamically

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -8,7 +8,7 @@ export interface LabelValue {
 
 export default function Sidebar({items}:any) {
   return (
-    <Container minW="2xs" >
+    <Container maxW="2xs">
       <Card minH="sm" m={3}>
       {items.map((item:any, index:any) => (
         <Link href={`/resources/${item.value}`} key={index}><Text size="xl" m={4}>{item.label}</Text></Link>
---
+++ pages/resources/games.tsx
@@ -0,0 +1,56 @@
+import {
+  Container,
+  Text,
+  Flex,
+  Box,
+  Link,
+  Spacer,
+  Card,
+  Image,
+  Stack,
+  Heading,
+  CardBody,
+  CardFooter,
+  ButtonGroup,
+  Button,
+} from "@chakra-ui/react";
+import Sidebar from "../../components/sidebar"
+
+const games = [
+  { image: "/assets/game.png", alt:"", title: "Snowball Thrower", description: "Practice your spelling with this enthralling game!", id: "1" },
+]
+
+export default function Games() {
+  return (
+    <Container minW="3xl">
+      {/* <Sidebar /> */}
+        <Flex flexWrap="wrap">
+          {games.map((game:any) => (
+          <Card maxW="xs" m={5} key={game.id}>
+            <CardBody>
+              <Image
+                src={game.image}
+                alt={game.alt}
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">{game.title}</Heading>
+                <Text>{game.description}</Text>
+              </Stack>
+            </CardBody>
+            <CardFooter>
+              <ButtonGroup spacing="2">
+                <Link href={`/resources/games/${game.id}`}>
+                  <Button variant="solid" backgroundColor="teal">
+                    Play
+                  </Button>
+                </Link>
+              </ButtonGroup>
+            </CardFooter>
+          </Card>
+          ))}
+
+        </Flex>
+    </Container>
+  );
+}
